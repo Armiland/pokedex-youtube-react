6ed472a2f3384c8c8dc99385010f1c7b35334d96
Lauren Armiland <133688012+Armiland@users.noreply.github.com>
2023-07-16 16:45:52 +0100

    pokedexing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Routes, Route, Switch}
+from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 import NavBar from './components/layout/NavBar';
 import Dashboard from './components/layout/Dashboard';
+import Pokemon from './components/pokemon/Pokemon';
+
+import backgroundImage from '../src/components/pattern.png';
 
 class App extends Component {
   render() {
   return (
-    <div className="App">
+    <Router>
+    <div className="App" style={{background: `url(${backgroundImage})`}}>
       <NavBar />
       <div className="container">
+        <switch>
+         <Routes><Route exact path="/" Component={Dashboard} />
+          <Route exact path="/pokemon/:pokemonIndex" Component={Pokemon} />
+       </Routes> </switch>
         <Dashboard />
         
         </div>
     </div>
+    </Router>
   );
 }
 }
